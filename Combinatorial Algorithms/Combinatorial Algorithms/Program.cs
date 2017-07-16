346f34e78c41fb28e1d1c880ec330209532bb2f9
Petar Stoyanov <pesho.stoyanov@gmail.com>
2017-07-16 18:48:32 +0300

    cleaner gitignore

---
+++ Combinatorial Algorithms/Combinatorial Algorithms/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Combinatorial_Algorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split().ToArray();
+
+            GeneratePermutations(input);
+
+        }
+
+        private static void GeneratePermutations(string[] input)
+        {
+            bool[] used = new bool[input.Length];
+
+            string[] permutation = new string[input.Length];
+
+            GeneratePermutations(input, permutation, used, 0);
+        }
+
+        private static void GeneratePermutations(int index)
+        {
+            //base case
+            if (index >= input.Length)
+            {
+                Console.WriteLine(string.Join(" ", permutation));
+            }
+            else
+            {
+                for (int i = 0; i < input.Length; i++)
+                {
+                    if (!used[i])
+                    {
+                        used[i] = true;
+                        permutation[index] = input[i];
+                        GeneratePermutations(input, permutation, used, index + 1);
+                        used[i] = false;
+                    }
+                }
+            }
+        }
+    }
+}
