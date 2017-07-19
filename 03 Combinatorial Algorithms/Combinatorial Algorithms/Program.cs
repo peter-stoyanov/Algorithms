60b7f75693be7d6d8915b4316dc0a55627e55287
Petar Stoyanov <pesho.stoyanov@gmail.com>
2017-07-19 08:01:55 +0300

    new exercises

---
+++ 03 Combinatorial Algorithms/Combinatorial Algorithms/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Combinatorial_Algorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var inputStrings = Console.ReadLine().Split().ToArray();
+            var inputNumbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            //GeneratePermutations(inputStrings);
+
+            Permute(inputNumbers);
+        }
+
+        // Algorithm with additional bool array
+        private static void GeneratePermutations(string[] input)
+        {
+            bool[] used = new bool[input.Length];
+
+            string[] permutation = new string[input.Length];
+
+            GeneratePermutations(input, permutation, used, 0);
+        }
+
+        private static void GeneratePermutations(string[] input, string[] permutation, bool[] used, int index)
+        {
+            //base case
+            if (index >= input.Length)
+            {
+                Console.WriteLine(string.Join(" ", permutation));
+            }
+            else
+            {
+                for (int i = 0; i < input.Length; i++)
+                {
+                    if (!used[i])
+                    {
+                        used[i] = true;
+                        permutation[index] = input[i];
+                        GeneratePermutations(input, permutation, used, index + 1);
+                        used[i] = false;
+                    }
+                }
+            }
+        }
+
+        //algorithm with no additionam memory req. and no add. arrays
+        private static void Permute(int[] arr, int startIndex = 0)
+        {
+            if (startIndex >= arr.Length - 1)
+            {
+                Console.WriteLine(string.Join(" ", arr));
+            }
+            else
+            {
+                for (int i = startIndex; i < arr.Length; i++)
+                {
+                    Swap(ref arr[startIndex], ref arr[i]);
+                    Permute(arr, startIndex + 1);
+                    Swap(ref arr[i], ref arr[startIndex]);
+                }
+            }
+        }
+
+        private static void Swap(ref int i, ref int j)
+        {
+            if (i == j)
+            {
+                return;
+            }
+
+            i ^= j;
+            j ^= i;
+            i ^= j;
+        }
+    }
+}
---
+++ 04 Greedy Lab/SumOfCoins/SumOfCoins.cs
@@ -0,0 +1,64 @@
+﻿namespace SumOfCoins
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class SumOfCoins
+    {
+        public static void Main(string[] args)
+        {
+            var availableCoins = new[] { 1, 2, 5, 10, 20, 50 };
+            var targetSum = 923;
+
+            var selectedCoins = ChooseCoins(availableCoins, targetSum);
+
+            Console.WriteLine($"Number of coins to take: {selectedCoins.Values.Sum()}");
+            foreach (var selectedCoin in selectedCoins)
+            {
+                Console.WriteLine($"{selectedCoin.Value} coin(s) with value {selectedCoin.Key}");
+            }
+        }
+
+        public static Dictionary<int, int> ChooseCoins(IList<int> coins, int targetSum)
+        {
+            var selectedCoins = new Dictionary<int, int>();
+
+            //sort so its easy to take the biggest available coin
+            var availableCoins = coins.OrderBy(c => -c).ToList();
+
+            int proposedCoin = 0;
+            while (availableCoins.Count > 0)
+            {
+                proposedCoin = availableCoins.FirstOrDefault();
+
+                if (targetSum >= proposedCoin)
+                {
+                    //add to selected coins
+                    if (selectedCoins.ContainsKey(proposedCoin))
+                    {
+                        selectedCoins[proposedCoin]++;
+                    }
+                    else
+                    {
+                        selectedCoins.Add(proposedCoin, 1);
+                    }
+
+                    targetSum -= proposedCoin;
+                }
+                else
+                {
+                    //remove from available coins
+                    availableCoins.Remove(proposedCoin);
+                }
+            }
+
+            if (targetSum > 0)
+            {
+                throw new InvalidOperationException("Greedy algorithm can not produce desired sum with available coins.");
+            }
+
+            return selectedCoins;
+        }
+    }
+}
