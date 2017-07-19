c2ef9acbfddae806cfded759f7b5f098c1a9e350
Petar Stoyanov <pesho.stoyanov@gmail.com>
2017-07-19 22:24:45 +0300

    namespaces deleted from lab

--- 04 Greedy Lab/SumOfCoins/SumOfCoins.cs
+++ 04 Greedy Lab/SumOfCoins/SumOfCoins.cs
@@ -1,64 +1,61 @@
-﻿namespace SumOfCoins
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class SumOfCoins
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-
-    public class SumOfCoins
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        var availableCoins = new[] { 1, 2, 5, 10, 20, 50 };
+        var targetSum = 923;
+
+        var selectedCoins = ChooseCoins(availableCoins, targetSum);
+
+        Console.WriteLine($"Number of coins to take: {selectedCoins.Values.Sum()}");
+        foreach (var selectedCoin in selectedCoins)
         {
-            var availableCoins = new[] { 1, 2, 5, 10, 20, 50 };
-            var targetSum = 923;
-
-            var selectedCoins = ChooseCoins(availableCoins, targetSum);
-
-            Console.WriteLine($"Number of coins to take: {selectedCoins.Values.Sum()}");
-            foreach (var selectedCoin in selectedCoins)
-            {
-                Console.WriteLine($"{selectedCoin.Value} coin(s) with value {selectedCoin.Key}");
-            }
+            Console.WriteLine($"{selectedCoin.Value} coin(s) with value {selectedCoin.Key}");
         }
+    }
 
-        public static Dictionary<int, int> ChooseCoins(IList<int> coins, int targetSum)
+    public static Dictionary<int, int> ChooseCoins(IList<int> coins, int targetSum)
+    {
+        var selectedCoins = new Dictionary<int, int>();
+
+        //sort so its easy to take the biggest available coin
+        var availableCoins = coins.OrderBy(c => -c).ToList();
+
+        int proposedCoin = 0;
+        while (availableCoins.Count > 0)
         {
-            var selectedCoins = new Dictionary<int, int>();
+            proposedCoin = availableCoins.FirstOrDefault();
 
-            //sort so its easy to take the biggest available coin
-            var availableCoins = coins.OrderBy(c => -c).ToList();
-
-            int proposedCoin = 0;
-            while (availableCoins.Count > 0)
+            if (targetSum >= proposedCoin)
             {
-                proposedCoin = availableCoins.FirstOrDefault();
-
-                if (targetSum >= proposedCoin)
+                //add to selected coins
+                if (selectedCoins.ContainsKey(proposedCoin))
                 {
-                    //add to selected coins
-                    if (selectedCoins.ContainsKey(proposedCoin))
-                    {
-                        selectedCoins[proposedCoin]++;
-                    }
-                    else
-                    {
-                        selectedCoins.Add(proposedCoin, 1);
-                    }
-
-                    targetSum -= proposedCoin;
+                    selectedCoins[proposedCoin]++;
                 }
                 else
                 {
-                    //remove from available coins
-                    availableCoins.Remove(proposedCoin);
+                    selectedCoins.Add(proposedCoin, 1);
                 }
-            }
 
-            if (targetSum > 0)
+                targetSum -= proposedCoin;
+            }
+            else
             {
-                throw new InvalidOperationException("Greedy algorithm can not produce desired sum with available coins.");
+                //remove from available coins
+                availableCoins.Remove(proposedCoin);
             }
-
-            return selectedCoins;
         }
+
+        if (targetSum > 0)
+        {
+            throw new InvalidOperationException("Greedy algorithm can not produce desired sum with available coins.");
+        }
+
+        return selectedCoins;
     }
 }
