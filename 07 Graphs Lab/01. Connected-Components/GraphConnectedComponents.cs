b2ea8c216fd6df8b66cb906c87727d66bd74ff31
Petar Stoyanov <pesho.stoyanov@gmail.com>
2017-07-30 11:35:07 +0300

    components and topological sorting lab

---
+++ 07 Graphs Lab/01. Connected-Components/GraphConnectedComponents.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class GraphConnectedComponents
+{
+    static List<int>[] graph;
+
+    public static void Main()
+    {
+        graph = ReadGraph();
+        FindGraphConnectedComponents();
+    }
+
+    private static List<int>[] ReadGraph()
+    {
+        int n = int.Parse(Console.ReadLine());
+        var graph = new List<int>[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            graph[i] = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToList();
+        }
+        
+        return graph;
+    }
+
+    private static void FindGraphConnectedComponents()
+    {
+        bool[] visited = new bool[graph.Length];
+        int[] componentIds = new int[graph.Length];
+        int count = 0;
+
+        for (int i = 0; i < graph.Length; i++)
+        {
+            DFS(graph[i], i, visited, count, componentIds);
+            count++;
+        }
+        var vertexesByComponents = componentIds
+            .Select((v, i) => new { vertex = v, index = i })
+            .GroupBy(node => node.vertex)
+            .Select(group => new { componentId = group.Key, vertexes = group })
+            .ToList();
+
+        foreach (var componentGroup in vertexesByComponents)
+        {
+            Console.WriteLine($"Connected component: {String.Join(" ", componentGroup.vertexes.Select(v => v.index))}");
+        }
+    }
+
+    private static void DFS(List<int> node, int index, bool[] visited, int count, int[] componentIds)
+    {
+        if (!visited[index])
+        {
+            visited[index] = true;
+            componentIds[index] = count;
+            foreach (var child in node)
+            {
+                DFS(graph.Where((g, i) => i == child).FirstOrDefault(), child, visited, count, componentIds);
+            }
+        }
+    }
+}
---
+++ 07 Graphs Lab/02. Topological-Sorting/TopologicalSorter.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class TopologicalSorter
+{
+    private Dictionary<string, List<string>> graph;
+
+    public TopologicalSorter(Dictionary<string, List<string>> graph)
+    {
+        this.graph = graph;
+    }
+
+    public ICollection<string> TopSort()
+    {
+        //preprocess -> predecessors by vertex
+        var predecessorsCount = new Dictionary<string, int>();
+
+        foreach (var sourceTargetPair in graph)
+        {
+            if (!predecessorsCount.ContainsKey(sourceTargetPair.Key))
+            {
+                predecessorsCount[sourceTargetPair.Key] = 0;
+            }
+
+            foreach (var target in sourceTargetPair.Value)
+            {
+                if (predecessorsCount.ContainsKey(target))
+                {
+                    predecessorsCount[target]++;
+                }
+                else
+                {
+                    predecessorsCount.Add(target, 1);
+                }
+            }
+        }
+        //find V with no parents
+        var sortedGraph = new List<string>();
+
+        while (true)
+        {
+            var nodeWithoutSource = predecessorsCount.Keys
+                .Where(x => predecessorsCount[x] == 0).FirstOrDefault();
+
+            if (nodeWithoutSource != null)
+            {
+                //adjust predecessors count on children
+                foreach (var orphan in graph[nodeWithoutSource])
+                {
+                    predecessorsCount[orphan]--;
+                }
+
+                //remove it
+                graph.Remove(nodeWithoutSource);
+                predecessorsCount.Remove(nodeWithoutSource);
+
+                //add removed node to sort order
+                sortedGraph.Add(nodeWithoutSource);
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        //if in the end stil exists V's in graph - > exception for cycle
+        if (graph.Any())
+        {
+            throw new InvalidOperationException("Graph has cycle.");
+        }
+
+        return sortedGraph;
+    }
+}
